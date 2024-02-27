d09b6b3936430678e9f29221a5c819d8c03d592e
soh3illl <soul.abb2002@gmail.com>
2024-02-27 21:16:24 +0330

    remove static keyword for methods and fields

--- src/main/java/org/example/bank/Bank.java
+++ src/main/java/org/example/bank/Bank.java
@@ -6,21 +6,21 @@ import java.util.List;
 import java.util.Map;
 
 public class Bank {
-    private static Map<String, BankAccount> accounts = new HashMap<>();
+    private Map<String, BankAccount> accounts = new HashMap<>();
 
-    public static void addAccount(BankAccount account) {
+    public void addAccount(BankAccount account) {
         accounts.put(account.getAccountNumber(), account);
     }
 
-    public static void removeAccount(String accountNumber) {
+    public void removeAccount(String accountNumber) {
         accounts.remove(accountNumber);
     }
 
-    public static BankAccount findAccount(String accountNumber) {
+    public BankAccount findAccount(String accountNumber) {
         return accounts.get(accountNumber);
     }
 
-    public static List listAccounts() {
+    public List listAccounts() {
         return new ArrayList(accounts.values());
     }
 }
